eae18d37bc3d86104bff6fde7bfc9a429bb46504
Aleks Petrov <hello@alekspetrov.com>
2022-12-07 14:03:25 +0100

    updates in content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          indigo: "#6556C5",
+          pink: "#DF34BC",
+          bg: "#0B0F19",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
