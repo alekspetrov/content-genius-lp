4f02a882027c21e50bbf51596600973a9b79a696
Aleks Petrov <hello@alekspetrov.com>
2022-12-08 14:59:43 +0100

    add policies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), image()],
+  integrations: [tailwind(), sitemap(), image(), mdx()]
 });
