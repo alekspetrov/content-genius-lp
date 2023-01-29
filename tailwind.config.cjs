ddfb675b10941b1d533fe20fa9db62d0ae3b6d75
Aleks Petrov <hello@alekspetrov.com>
2023-01-29 03:39:22 +0100

    progress

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,35 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
       colors: {
-        brand: {
-          indigo: "#6556C5",
-          pink: "#DF34BC",
-          bg: "#0B0F19",
+        graphite: {
+          900: "#090809",
+          800: "#121212",
+          700: "#181818",
+          600: "#1F1F1F",
+          500: "#2f2f2f",
+          400: "#A0A0A0",
+          300: "#BDBDBD",
+          200: "#D9D9D9",
+          100: "#F8F8F8",
         },
+        brand: {
+          indigo: "#7C00AC",
+          pink: "#D7009B",
+          success: "#00D796",
+          warning: "#F5A623",
+          alert: "#D74D00",
+        },
+      },
+      backgroundImage: {
+        "gradient-radial-indigo":
+          "radial-gradient(50% 50% at 50% 50%, #7c00ac 0%, #090809 100%)",
+      },
+      backgroundSize: {
+        "50%": "50% 50%",
       },
     },
   },
