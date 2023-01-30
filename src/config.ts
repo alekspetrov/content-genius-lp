36e13939adccbc653c5f88ea2fcf3943bac78890
Aleks Petrov <hello@alekspetrov.com>
2023-01-30 13:30:11 +0100

    config upd

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,10 @@
 export const SUPABASE_PUBLIC_URL = "https://eathcybqadpvaknyrpsb.supabase.co";
 export const SUPABASE_PUBLIC_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhdGhjeWJxYWRwdmFrbnlycHNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc0OTAzMTMsImV4cCI6MTk4MzA2NjMxM30.zeWfDRE_mRWl3KnrcBvbHv_GLfU3fcOFy7yJj0hzFiM";
+export const SITE_URL = "https://contentgenius.io";
+export const SITE_TITLE = "Content Genius";
+export const SITE_OG_IMAGE_URL =
+  "https://contentgenius.io/og_default_image.png";
+export const CURRENT_LOCATION = "Central EU (GMT+1)";
+export const SITE_DESCRIPTION =
+  "Content Genius is an all-in-one modern platform for content creation and distribution.";
