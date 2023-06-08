e604d138e0c5300f3b4f78c07f6b907e507cda9e
Aleks Petrov <hello@alekspetrov.com>
2023-06-08 14:31:11 +0200

    upd og info

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SUPABASE_PUBLIC_URL = "https://eathcybqadpvaknyrpsb.supabase.co";
 export const SUPABASE_PUBLIC_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhdGhjeWJxYWRwdmFrbnlycHNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc0OTAzMTMsImV4cCI6MTk4MzA2NjMxM30.zeWfDRE_mRWl3KnrcBvbHv_GLfU3fcOFy7yJj0hzFiM";
-export const SITE_URL = "https://contentgenius.io";
+export const SITE_URL = "https://contentgenius.io/";
 export const SITE_TITLE = "Content Genius";
 export const SITE_OG_IMAGE_URL = "https://contentgenius.io/og_default_img.png";
 export const CURRENT_LOCATION = "Central EU (GMT+1)";
