0a78d0417a522af9bc8adc321c542fbaff764241
Aleks Petrov <hello@alekspetrov.com>
2023-06-08 14:22:23 +0200

    upd og_image

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,7 @@ export const SUPABASE_PUBLIC_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhdGhjeWJxYWRwdmFrbnlycHNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc0OTAzMTMsImV4cCI6MTk4MzA2NjMxM30.zeWfDRE_mRWl3KnrcBvbHv_GLfU3fcOFy7yJj0hzFiM";
 export const SITE_URL = "https://contentgenius.io";
 export const SITE_TITLE = "Content Genius";
-export const SITE_OG_IMAGE_URL =
-  "https://contentgenius.io/og_default_image.png";
+export const SITE_OG_IMAGE_URL = "https://contentgenius.io/og_default_img.png";
 export const CURRENT_LOCATION = "Central EU (GMT+1)";
 export const SITE_DESCRIPTION =
   "AI-Powered Content Studio. For copywriters, marketers, social media managers and more.";
