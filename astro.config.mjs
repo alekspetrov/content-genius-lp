bd0a5f39d0ee179292128ebdd872799397982990
nf404 <nf404@users.noreply.github.com>
2023-08-14 18:13:17 +0200

    improve sitemap and add automatic robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
+import robotsTxt from 'astro-robots-txt';
 
 import solidJs from "@astrojs/solid-js";
 
@@ -15,9 +16,33 @@ import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), sitemap(), image(), mdx(), solidJs(), partytown()]
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    sitemap({
+      filter: (page) =>
+        page !== `${SITE_URL}early-adopters-discount/`,
+      serialize(item) {
+        if (item.url === `${SITE_URL}`) {
+          item.priority = 1;
+          return item
+        }
+        if (item.url === `${SITE_URL}blog/`) {
+          item.priority = 1;
+          return item
+        }
+        item.priority = 0.8;
+        return item
+      },
+      changefreq: 'hourly',
+    }),
+    image(),
+    mdx(),
+    solidJs(),
+    partytown(),
+    robotsTxt(),
+  ],
 });
