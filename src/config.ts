cb8e7a9c366c8e4a8e8f1886f53b599b01a28b71
Aleks Petrov <mr.alex.petrov@gmail.com>
2023-08-24 15:35:26 +0200

    upd og image name to reset cache

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const SUPABASE_PUBLIC_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhdGhjeWJxYWRwdmFrbnlycHNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc0OTAzMTMsImV4cCI6MTk4MzA2NjMxM30.zeWfDRE_mRWl3KnrcBvbHv_GLfU3fcOFy7yJj0hzFiM";
 export const SITE_URL = "https://contentgenius.io/";
 export const SITE_TITLE = "Content Genius";
-export const SITE_OG_IMAGE_URL = "og_default_img.png";
+export const SITE_OG_IMAGE_URL = "og_default_1_img.png";
 export const CURRENT_LOCATION = "Central EU (GMT+1)";
 export const SITE_DESCRIPTION =
   "Get a better ChatGPT experience. Perfect for copywriters, marketers, social media managers, and professionals from various fields. No specific expertise is necessary.";
